950ed5f629bf9a9f04d5314c0afa76bb4cbda9a3
Gian Carlo Llanes Garcia <gian@brightbid.ai>
2024-04-14 13:06:08 +0800

    remove docker

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-const BASE_URL = 'http://localhost:8080'
-// const BASE_URL = 'https://fd41-136-158-79-10.ngrok-free.app'
+// const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://octopus-app-qxzob.ondigitalocean.app'
 
 export default BASE_URL
