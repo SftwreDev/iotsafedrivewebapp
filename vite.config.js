e9895695357e914d3b2213d8109df1c408fb2ff1
Gian Carlo Llanes Garcia <gian@brightbid.ai>
2024-04-17 21:40:15 +0800

    added pwa

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,38 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+            },
+            manifest: {
+                'theme_color': '#00948f',
+                'background_color': '#f7f8f8',
+                'icons': [{
+                    'purpose': 'maskable',
+                    'sizes': '512x512',
+                    'src': 'icon512_maskable.png',
+                    'type': 'image/png'
+                }, { 'purpose': 'any', 'sizes': '512x512', 'src': 'icon512_rounded.png', 'type': 'image/png' }],
+                'orientation': 'portrait',
+                'display': 'standalone',
+                'dir': 'auto',
+                'lang': 'en-US',
+                'name': 'IotSafeDrive',
+                'short_name': 'iotsafedrive'
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
